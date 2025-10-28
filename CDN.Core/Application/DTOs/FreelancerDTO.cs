7fbd2ca59728d9c68c72c92b12e76118cc1cd3e1
khairulazanni <khairul.azanni@gmail.com>
2025-10-28 16:33:40 +0800

    feat: create freelancer DTO

---
+++ CDN.Core/Application/DTOs/FreelancerDTO.cs
@@ -0,0 +1,58 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CDN.Core.Application.DTOs
+{
+    public class CreateFreelancerDto
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public List<string> SkillSet { get; set; } = new();
+
+        public List<string> Hobbies { get; set; } = new();
+    }
+
+    public class UpdateFreelancerDto
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string? PhoneNumber { get; set; }
+
+        public List<string> SkillSet { get; set; } = new();
+
+        public List<string> Hobbies { get; set; } = new();
+    }
+
+    public class freelancerresponseDto
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public List<string> SkillSet { get; set; } = new();
+        public List<string> Hobbies { get; set; } = new();
+    }
+
+    public class SearchFreelancerDto
+    {
+        [Required]
+        public string SearchQuery { get; set; } = string.Empty;
+    }
+}
